30c8a3fa3a11ba2d6ad64e4c69aa2897044613fb
Adriano Carrijo <adrianovieirac@gmail.com>
2024-04-30 15:26:36 -0300

    add laravel pulse configuration
    TASK #6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production')) {
+            Gate::define('viewPulse', function (User $user) {
+                return $user->isAdmin();
+            });
+        }
     }
 }
